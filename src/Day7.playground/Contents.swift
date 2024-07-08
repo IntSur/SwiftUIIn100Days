ec699656d716252f31791ba3b9392ef11ea1e1ae
fanjiahui <1203394464@qq.com>
2024-07-08 22:25:12 +0800

    update to Day8

---
+++ src/Day7.playground/Contents.swift
@@ -0,0 +1,66 @@
+import Cocoa
+
+//比较两个字符串内容是否相等
+func compareTwoStrings(String1: String, String2: String) -> Bool {
+//    let first = String1.sorted()
+//    let second = String2.sorted()
+//    if first == second {
+//        return true
+//    } else {
+//        return false
+//    }
+//    以上代码可以简写成以下方式
+//    return String1.sorted() == String2.sorted()
+//    只有一行返回值代码时 可以把return省略
+    String1.sorted() == String2.sorted()
+}
+
+compareTwoStrings(String1: "abcd", String2: "adcb")
+
+func Pythagorean(a: Double,b: Double) -> Double {
+//    var input = a * a + b * b
+//    return sqrt(input)
+    sqrt(a * a + b * b)
+}
+
+Pythagorean(a: 3, b: 4)
+
+//元组
+//元组全写形式
+func tuplePrint1() -> (firstname: String, secondname: String) {
+//    (firstname:"Steve", secondname:"Jobs")
+//    可以不写元组键
+    ("Steve","Jobs")
+}
+let ceo1 = tuplePrint1()
+print("\(ceo1.firstname) \(ceo1.secondname)")
+//元组数组模式
+func tuplePrint2() -> (String, String) {
+    ("Steve","Jobs")
+}
+let ceo2 = tuplePrint2()
+print("\(ceo2.0) \(ceo2.1)")
+//元组分裂赋值
+func tuplePrint3() -> (firstname:String, secondname:String){
+    (firstname:"Steve", secondname:"Jobs")
+}
+let (firstname1, secondname) = tuplePrint3()
+let (firstname2, _) = tuplePrint3()
+print("\(firstname1) \(secondname)")
+print("I just want firstname:\(firstname1)")
+
+func rollDices(sides: Int, count: Int) -> [Int] {
+    var dicePoints = [Int]()
+    for _ in 1...count {
+        dicePoints.append(Int.random(in: 1...sides))
+    }
+    return dicePoints
+}
+print(rollDices(sides: 6, count: 4))
+
+//参数标签和参数名称：函数参数可以起两个名字，一个给外部调用时使用，一个给内部使用。
+func isUppercase(_ string: String) -> Bool {
+    string == string.uppercased()
+}
+let string1 = "HELLO,WOLRD!"
+print(isUppercase(string1))
---
+++ src/Day8.playground/Contents.swift
@@ -0,0 +1,91 @@
+import Cocoa
+
+//函数能给默认值
+func rollDices(sides: Int, count: Int = 5) -> [Int] {
+    var dicePoints = [Int]()
+    for _ in 1...count {
+        dicePoints.append(Int.random(in: 1...sides))
+    }
+    return dicePoints
+}
+print(rollDices(sides: 6, count: 4))
+
+var demoArray1 = ["1", "2", "3", "4"]
+print(demoArray1.count)
+//keepingCapacity:清空数组后，是否保存数组原内存空间的开关，默认是关着的。
+demoArray1.removeAll(keepingCapacity: true)
+print(demoArray1.count)
+
+//对可能发生错误的代码段抓错：do{...}catch{...}
+//1.创建出一个error类型的枚举
+enum ErrorCodes:Error {
+    case short
+    case obvious
+}
+//2.设置抛错条件
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {throw ErrorCodes.short}
+    if password == "12345" {throw ErrorCodes.obvious}
+    
+    if password.count > 5 {
+        return "Password length: Normal"
+    } else if password.count < 12 {
+        return "Password length: Good"
+    } else {
+        return "Password length: Great"
+    }
+}
+
+//3.do{}catch{} try是设置抓错的关键词
+do {
+    let password = "123"
+    let result = try checkPassword(password)//try在这里可以写成try!（同时也不用写do了）：用来表示完全确认不会抛错，很少用！
+    print(result)
+} catch {
+    print("Error happend")
+}
+
+//4.自定义抛错处理
+do {
+    let password = "12345"
+    let result = try checkPassword(password)
+    print(result)
+} catch ErrorCodes.short {
+    print("Password too short!")
+} catch ErrorCodes.obvious {
+    print("Password too obvious!")
+} catch {
+    print(error.localizedDescription)//苹果自带的报错信息
+}
+
+//CheckPoint5📋：自己写一个1～10000求平方根的函数
+enum ErrorCodeForSqrt:Error {
+    case tooLow
+    case tooHigh
+    case noFound
+}
+
+func squareRoot(_ numberToBeSquared:Int) throws -> Int {
+    if numberToBeSquared < 1 { throw ErrorCodeForSqrt.tooLow }
+    if numberToBeSquared > 10_000 { throw ErrorCodeForSqrt.tooHigh }
+    for i in 1...100 {
+        if i * i == numberToBeSquared {
+            return i
+        }
+    }
+    throw ErrorCodeForSqrt.noFound
+}
+
+let number = 10000
+do {
+    let result = try squareRoot(number)
+    print("The root of \(number) is \(result)")
+} catch ErrorCodeForSqrt.tooLow {
+    print("\(number) is too low.")
+} catch ErrorCodeForSqrt.tooHigh {
+    print("\(number) is too high.")
+} catch ErrorCodeForSqrt.noFound {
+    print("Root no found")
+} catch {
+    print("A problem happend")
+}
